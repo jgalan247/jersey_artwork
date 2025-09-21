f675131377341fa67f96de435820f1a366a16ca5
DrGalan <jgalan247@gmail.com>
2025-09-21 18:26:04 +0100

    Update urls.py
    Short term fix to display images

--- artworks/urls.py
+++ artworks/urls.py
@@ -1,21 +1,26 @@
-# artworks/urls.py  (PROJECT ROOT router)
+# artworks/urls.py (PROJECT ROOT router)
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('orders/', include(('orders.urls', 'orders'), namespace='orders')),
     path('payments/', include(('payments.urls', 'payments'), namespace='payments')),
     path('subscriptions/', include(('subscriptions.urls', 'subscriptions'), namespace='subscriptions')),
-
     path("", include(("artworks.app_urls", "artworks"), namespace="artworks")),
-
 ]
 
+# Serve media files ALWAYS - for demo purposes
+# This works in both DEBUG=True and DEBUG=False
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Still handle static files properly
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
