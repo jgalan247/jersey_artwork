7ed6ecbc2a0e42a38c80ca519b74afefabd85343
DrGalan <>
2025-10-05 15:52:19 +0100

    Enable SMTP email sending with Gmail

--- artworks/settings_demo.py
+++ artworks/settings_demo.py
@@ -150,7 +150,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Jersey Artwork <noreply@coderra.je>')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Jersey Artwork <admin@coderra.je>')
 EMAIL_SUBJECT_PREFIX = '[Jersey Artwork] '
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
